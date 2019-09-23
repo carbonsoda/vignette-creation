b25c246548beb81937632642b66b38045ba0e16f
Avery <avery@uconn.edu>
2019-09-23 16:07:14 -0400

    Bug fixes + process on Batch mode
    Fixed some issues with the vignettes mode.
    
    Working on batch mode, have general methods + ui configured (including formatting message box).
    Need to make mock csvs and run tests.

--- logic.py
+++ logic.py
@@ -1,5 +1,5 @@
 import os, csv, subprocess, re
-from collections import namedtuple
+from collections import defaultdict
 from pydub import AudioSegment
 
 
@@ -7,6 +7,7 @@ class VideoLogic:
 
     def __init__(self):
         self.defaultlength = 0.5  # 0.5 seconds
+        self.defaultbeeptime = 30  # 30 seconds in
         self.beepfile = "vignette_beep.wav"
 
         self.beep_savepath = ''
@@ -14,7 +15,13 @@ class VideoLogic:
     # goes through csv file of times + generates time segments
     def readinput(self, videopath, savebeeppath, csvfile):
         audiofile = AudioSegment.empty()
+        correctlength = self.get_video_length(videopath)
+
         try:
+
+            if len(csvfile) < 5:
+                return self.defaultdurationbeep(videopath)
+
             with open(csvfile, 'r+') as f:
                 csv_input = csv.reader(f, delimiter=',')
 
@@ -36,21 +43,21 @@ class VideoLogic:
                                 time = (float(row[i]), float(row[i]) + self.defaultlength)
                                 hastime = True
                     if hastime:
-                        if silentstart == 0:
-                            if not time[0]:  # beep starts @ t=0.0
-                                silentstart = time[1]
+                        # beep starts @ t = 0
+                        if silentstart == 0 and time[0] == 0:
+                            silentstart = time[1]
                         else:
-                            silentend = time[1]
-                            audiofile += self.audiogenerator((silentstart, silentend))
+                            # silence starts @ 0
+                            silentend = time[0]
+                            audiofile += self.audiogenerator((silentstart, silentend), isbeep=False)
                             # for next row
-                            silentstart = time[0]
+                            silentstart = time[1]
 
                         audiofile += self.audiogenerator(time, True)
 
-            if audiofile.duration_seconds > 1:
-                correctlength = self.get_video_length(audiofile.duration_seconds, videopath)
-                if correctlength:
-                    silence = correctlength * 1000
+            if audiofile.duration_seconds > 0:
+                if correctlength > audiofile.duration_seconds:
+                    silence = (correctlength - audiofile.duration_seconds) * 1000
                     audiofile += AudioSegment.silent(duration=silence)
 
                 with open(self.beep_savepath, 'w+') as f:
@@ -60,6 +67,24 @@ class VideoLogic:
             # self.permissionerrorbox()
             return None
 
+    def defaultdurationbeep(self, videopath):
+        audiofile = AudioSegment.empty()
+        audiofile += self.audiogenerator((0, 30), isbeep=False)
+        audiofile += self.audiogenerator((30, 30 + self.defaultlength), True)
+
+        if audiofile.duration_seconds > 0:
+            correctlength = self.get_video_length(videopath)
+            if correctlength > audiofile.duration_seconds:
+                silence = (correctlength - audiofile.duration_seconds) * 1000
+                audiofile += AudioSegment.silent(duration=silence)
+            try:
+                with open(self.beep_savepath, 'w+') as f:
+                    audiofile.export(self.beep_savepath, format="wav")
+                return os.path.exists(self.beep_savepath)
+            except (PermissionError, OSError) as e:
+                return None
+
+    # Generates beep audiosegments
     def audiogenerator(self, time, isbeep=False):
         # in milliseconds
         # seperate *1000 to avoid Decimal import https://floating-point-gui.de/languages/python/
@@ -68,16 +93,17 @@ class VideoLogic:
         timespan = (time[1] * 1000) - (time[0] * 1000)
 
         if isbeep:
-            song = AudioSegment.from_file(self.beepfile)
-            beep = song[:timespan]  # after testing, can return just this line
+            beep = AudioSegment.from_file(self.beepfile)[:timespan]
             return beep
         else:
             return AudioSegment.silent(duration=timespan)
 
-    # to determine length of the wav file
-    def get_video_length(self, audiolength, path):
-        process = subprocess.Popen([r'ffmpeg', '-i', path], stdout=subprocess.PIPE,
-                                   stderr=subprocess.STDOUT, shell=True)
+    # For purpose of determining length of the wave file
+    # make sure that the lengths match up
+    def get_video_length(self, path):
+        process = subprocess.Popen([r'ffmpeg', '-i', path],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT)
         stdout, stderr = process.communicate()
 
         regex = r'Duration: (?P<hours>\d+?):(?P<minutes>\d+?):(?P<seconds>\d+\.\d+)'
@@ -89,17 +115,19 @@ class VideoLogic:
 
         vidtime = (60 * 60 * hr) + (60 * minut) + sec
 
-        if audiolength < vidtime:
-            return vidtime - audiolength
-
-        return 0
+        return vidtime
 
     # runs ffmpeg commands for making muted copy + beeped video
     def generatevignette(self, rawvideopath, mutedpath, vignettevideopath, audiopath):
 
         mutedprocess = subprocess.Popen(
-            [r'ffmpeg', '-i', rawvideopath, '-c', 'copy', '-an', mutedpath],
-            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True
+            [
+                r'ffmpeg',
+                '-i', rawvideopath,
+                '-c', 'copy', '-an', mutedpath
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT
         )
         stdout, stderr = mutedprocess.communicate()  # acts as a timer function
         if not os.path.exists(mutedpath):
@@ -114,8 +142,7 @@ class VideoLogic:
                         vignettevideopath
                     ],
                     stdout=subprocess.PIPE,
-                    stderr=subprocess.STDOUT,
-                    shell=True
+                    stderr=subprocess.STDOUT
                 )
             stdout, stderr = process.communicate()  # acts as a timer function
             if os.path.exists(vignettevideopath):
@@ -125,22 +152,38 @@ class VideoLogic:
 
     # Trims videos and saves accordingly
     def trimmer(self, sourcepath, savepath, start, end):
+        starttime = str(start)
+        endtime = str(end)
         process = subprocess.Popen(
                 [
                     r'ffmpeg', '-i', sourcepath,
-                    '-ss', start,
-                    '-to', end,
+                    '-ss', starttime,
+                    '-to', endtime,
                     savepath
                 ],
                 stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT,
-                shell=True)
+                stderr=subprocess.STDOUT
+        )
 
         stdout, stderr = process.communicate()  # basically a timer
         if os.path.exists(savepath):
+            try:
+                # unsure if i should check for existing...
+                audioname = os.path.splitext(savepath)[0] + '.mp3'
+
+                process = subprocess.Popen(
+                    [
+                        r'ffmpeg', '-i', savepath,
+                        '-vn', '-acodec', 'mp3', audioname
+                    ],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT
+                )
+                stdout, stderr = process.communicate()  # basically a timer
+            except:
+                pass  # idk possible errors
             return True
-        else:
-            return False
+        return False
 
     def checkpaths(self, expectedpath):
         if os.path.exists(expectedpath):
@@ -177,116 +220,114 @@ class VideoLogic:
         else:
             return name
 
+    # makes the folder and places it into folder for later beep'ing
+    def foldergenerate(self, savepath, hasvignettepath = False):
+        oldsavepath = os.path.split(savepath) # (desired save location, filename_trimmed)
+        newsavepath = ''
 
-    # times are given in mm:ss:zzz, return as hh:mm:ss.zzz
-    # validates timestamps, otherwise calculate missing endstamp
-    # start CANNOT be considered "blank", ie if person wants from start of video!
-    def timegenerate(self, start, end, duration):
-        t1 = start
-        t2 = ''
+        if not hasvignettepath:
+            # avoids folder naming nightmare
+            folder, name = self.makevignettepath(savepath)
 
-        # missing end timestamps ( end == 00:00:000)
-        if set(int(n) for n in end.split(':')) == {0}:
-            durate = duration.split(":")
-            # if has duration
-            if set(int(n) for n in durate) != {0}:
-                for i, j in zip(start.split(":"), durate):
-                    k = int(i) + int(j)
-
-                    if len(str(k)) < 2:
-                        t2 += "0" + str(k) + ":"
-                    else:
-                        t2 += str(k) + ":"
-                if t2.endswith(":"):
-                    t2 = t2[:-1]
-            else:
-                return None  # no duration time given
+            if folder and name:
+                newsavepath = self.checkexisting(folder + os.sep + name, '_original')
+                return True, newsavepath
         else:
-            t2 = end
+            # Issue where after 1st trim, it doesn't generate it's own folder
+            # might have to just snip this all out, we'll see?
+            filename = str(oldsavepath[1].rsplit('_trimmed')[0])
+            newsavepath = self.checkexisting(oldsavepath[0] + os.sep + filename, '_original')
+            return hasvignettepath, newsavepath
 
-        while len(t1.split(":")) < 4:
-            t1 = "00:" + t1
-        while len(t2.split(":")) < 4:
-            t2 = "00:" + t2
 
-        try:
-            # ffmpeg requires 00:00:00.000
-            t1hhmmss, _, t1milli = t1.rpartition(":")
-            t2hhmmss, _, t2milli = t2.rpartition(":")
-            starttime = t1hhmmss + "." + t1milli
-            endtime = t2hhmmss + "." + t2milli
-            # can merge after testing
-            return starttime, endtime
-        except:
-            print(t1)
-            return None
+    # Converts mm:ss:zzz into purely seconds, NO hh:mm:ss:zzz
+    def timetoseconds(self, time):
+        seconds = 0
+        i = 2
 
-    def batchtrim(self, csvfile, savefolder):
-        # totrim {} == video_id : [sourcepath, start, end, savepath]
-        totrim = self._batchtrim(csvfile, savefolder)
-        failedtrim = []
+        for timepart in time.split(':'):
+            t = int(timepart)
 
-        if totrim:
-            for video_id in totrim:
-                start, end = self.timegenerate(totrim[video_id][1], totrim[video_id][2], "0")
+            if i == 2:
+                t = t * 60
+            elif i == 0:
+                t = round((t / 6000), 6)
+            elif i < 0:
+                print(timepart)
+                raise ValueError
+            seconds += t
+            i -= 1
 
-                if start and end:
-                    source = totrim[video_id][0]
-                    savepath = totrim[video_id][3]
+        return seconds
 
-                    if not self.trimmer(sourcepath=source, savepath=savepath, start=start, end=end):
-                        failedtrim.append(source)
-        else:
-            return None
+    # validates timestamps, otherwise calculate missing end-stamps
+    def timegenerate(self, start, end, timeduration):
+        starttime = self.timetoseconds(start)
+        endtime = self.timetoseconds(end)
 
-        print(failedtrim)
-        return failedtrim
+        if not endtime:  # missing END timestamp
+            endtime = self.timetoseconds(timeduration)
+            if not endtime: # no duration time given
+                return None
 
-    # self.trims = {}  # ID : name, beeppath, mutepath, vigpath
-    # helper method for batchtrim
-    # opens csvfile and loads contents + creates savepath for each
-    def _batchtrim(self, csvfile, savefolder):
-        totrim = {}
+        return starttime, endtime
+
+    # Parses each row in CSV
+    # returns list of lists
+    def batch_parsecsv(self, csvfile):
+        batch = []
+        bad = []
 
         try:
             with open(csvfile, 'r+') as f:
-                csv_input = csv.reader(f, delimiter=',')
+                csv_reader = csv.reader(f, delimiter=',')
+                next(csv_reader)
 
-                for row in csv_input:  # ID, Onset, Offset, File
-                    if len(row) < 4:
-                        pass
+                for line in csv_reader:
+                    # line = tuple(row)
+                    if 4 < len(line) < 3:
+                        bad.append(line)
                     else:
-                        try:
-                            video_id = row[0]
-                            onset = row[1]
-                            offset = row[2]
-                            source = row[3]
-
-                            totrim[video_id] = [source, onset, offset]
-                        except (IndexError, KeyError) as e:
-                            print(e)
-                            print(row)
-            # return totrim
+                        batch.append(line)
         except PermissionError:
             return None
+        print(bad)
+        return batch
 
-        if totrim:
-            for video_id in totrim:
-                try:
-                    id_folder = os.path.join(savefolder, video_id).replace("\\", "/")
-                    if not os.path.isdir(id_folder):
-                        os.makedirs(id_folder)
+    # batches = [[source, start, end]]
+    def batchtrim(self, batches):
+        failed = []
 
-                    savepath = self.checkexisting(id_folder + os.sep + video_id)
-                    if savepath:
-                        totrim[video_id].append(savepath)
-                except:
-                    print(video_id)
-            return totrim
+        for row in batches:
+            try:
+                source = row[0]
+                start = self.timetoseconds(row[1])
+                end = self.timetoseconds(row[2])
 
-    def batchvignette(self, mainfolder):
-        tobeep = []  # [(csvfile, beepfile, sourcevideo, mutedfile, vignettepath)]
-        failedbeeps = []
+                savepath = self.foldergenerate(source)
+
+                success = self.trimmer(source, savepath, start, end)
+                if not success:
+                    failed.append(row)
+            except ValueError:
+                failed.append(row)
+
+        return failed
+
+    def batchvignette(self, batches):
+        tobeep = []  # [sourcevideo, beeptimes)]
+        failed = []
+
+        for row in batches:
+            try:
+                pass
+            except ValueError:
+                failed.append(row)
+
+        try:
+            pass
+        except (PermissionError, IOError) as e:
+            return str(e)
 
         for path, dirs, files in os.walk(mainfolder):
             csvfile = ''
@@ -316,7 +357,7 @@ class VideoLogic:
             except IndexError:
                 print(tobeep[i])
 
-        return failedbeeps
+        return False, failedbeeps
 
     def _batchvignette(self, csvfile, beepfile, sourcevideo, mutedpath, vignettepath):
 
--- ui.py
+++ ui.py
@@ -154,7 +154,7 @@ class UiTrimmingTool:
         self.vigwindow.setWindowTitle('Vignette Tool')
         self.vigwindow.setWindowIcon(QtGui.QIcon("main.ico"))
         self.vigwindow.setFont(QtGui.QFont("Arial", 11))
-        self.vigwindow.setFixedSize(QtCore.QSize(500, 270))
+        self.vigwindow.setFixedSize(QtCore.QSize(500, 300))
 
         self.generatebtn = QtWidgets.QPushButton("Generate Vignette")
         self.viewfolderbtn = QtWidgets.QPushButton("View Vignette Folder")
@@ -163,8 +163,11 @@ class UiTrimmingTool:
 
         lyt = QtWidgets.QGridLayout(self.vigwindow)
         lyt.addWidget(self.vig_uploadbox(), 0, 0, 1, 2)
-        lyt.addWidget(self.generatebtn, 1, 0, 1, 1)
-        lyt.addWidget(self.viewfolderbtn, 1, 1, 1, 1)
+        # lyt.addWidget(questiondefaultbl, 1, 0, 1, 1)
+        # lyt.addWidget(defaultslbl, 1, 1, 1, 1)
+        lyt.addWidget(self.vig_defaultsbox(), 1, 0, 1, 2)
+        lyt.addWidget(self.generatebtn, 2, 0, 1, 1)
+        lyt.addWidget(self.viewfolderbtn, 2, 1, 1, 1)
 
     def vig_uploadbox(self):
         uploadbx = QtWidgets.QGroupBox("Upload...")
@@ -208,49 +211,144 @@ class UiTrimmingTool:
 
         return uploadbx
 
+    def vig_defaultsbox(self):
+        questionlbl = QtWidgets.QLabel("Start at a default time?")
+        # questionlbl.setFont(QtGui.QFont("Arial", 10))
+
+        self.vigdefault = QtWidgets.QCheckBox("Yes")
+        self.vigdefault.setChecked(True)
+        self.vigtime = QtWidgets.QLineEdit("30.00")
+        self.vigtime.setFixedWidth(60)
+
+        frame = QtWidgets.QFrame()
+        frame.setFont(QtGui.QFont("Arial", 10))
+        lyt = QtWidgets.QHBoxLayout(frame)
+        lyt.setSpacing(10)
+        lyt.addWidget(questionlbl)
+        lyt.addWidget(self.vigdefault)
+        # lyt.addItem(QtWidgets.QSpacerItem())
+        lyt.addWidget(self.vigtime)
+        lyt.addItem(QtWidgets.QSpacerItem(200,1))
+
+        return frame
+
+
     # BATCH MODE
     def batch_dialogbox(self, parent):
         self.batchwindow = QtWidgets.QDialog(parent)
 
         self.batchwindow.setWindowTitle('Batch Mode')
         self.batchwindow.setWindowIcon(QtGui.QIcon("batch.ico"))
-        self.batchwindow.setFont(QtGui.QFont("Arial", 11))
-        self.batchwindow.setFixedSize(QtCore.QSize(500, 200))
+        self.batchwindow.setFont(QtGui.QFont("Arial", 10))
+        self.batchwindow.setFixedSize(QtCore.QSize(400, 300))
 
         # MODE PICKER
-        checktrimlbl = QtWidgets.QLabel("Do you have a trimlist.csv?")
-        checkbeeplbl = QtWidgets.QLabel("Are there any beep csvs?")
-        self.trimchk = QtWidgets.QCheckBox("Yes")
-        self.beepchk = QtWidgets.QCheckBox("Yes")
+        rdbtnfont = QtGui.QFont("Arial", 9)
+        self.trimmoderdbtn = QtWidgets.QRadioButton('Trimming')
+        self.vignmoderdbtn = QtWidgets.QRadioButton('Vignettes (beeps)')
+        modebox = QtWidgets.QGroupBox('Which batch mode?')
+        modebox.setFixedHeight(70)
+        modeboxlyt = QtWidgets.QHBoxLayout(modebox)
+        modeboxlyt.addWidget(self.trimmoderdbtn)
+        modeboxlyt.addWidget(self.vignmoderdbtn)
 
-        # Guide label
-        trimlbl = QtWidgets.QLabel("trimlist.csv location:")
-        # UPLOAD BUTTON
-        self.trimfilebtn = self.file_btnsetup("Upload CSV")
-        # SAVE PATH LABEL
-        self.trimfilelbl = self.file_lblsetup()
+        # UPLOADING + GUIDE
+        uploadbox = self._batch_upload()
 
         # BEGIN BUTTON
-        self.beginbatchbtn = QtWidgets.QPushButton("Start Batch")
+        self.beginbatchbtn = QtWidgets.QPushButton('Start Batch')
         self.beginbatchbtn.setFixedHeight(30)
 
         # Add all to the layout
-        batchlyt = QtWidgets.QFormLayout(self.batchwindow)
-        batchlyt.setContentsMargins(9, 0, 9, 15)
-        batchlyt.setVerticalSpacing(9)
-        right = QtWidgets.QFormLayout.FieldRole  # for ease/more human sense
-        left = QtWidgets.QFormLayout.LabelRole
+        batchlyt = QtWidgets.QVBoxLayout(self.batchwindow)
 
-        batchlyt.setWidget(0, left, self.trimchk)
-        batchlyt.setWidget(0, right, checktrimlbl)
-        batchlyt.setWidget(1, right, checkbeeplbl)
-        batchlyt.setWidget(1, left, self.beepchk)
+        batchlyt.addWidget(modebox)
+        batchlyt.addWidget(uploadbox)
+        batchlyt.addWidget(self.beginbatchbtn)
 
-        batchlyt.setWidget(2, right, trimlbl)
-        batchlyt.setWidget(3, right, self.trimfilelbl)
-        batchlyt.setWidget(3, left, self.trimfilebtn)
+    # Groupbox containing file selector + guide btns
+    def _batch_upload(self):
+        uploadbox = QtWidgets.QGroupBox('Batch File Upload')
+        uploadbox.setFixedHeight(130)
 
-        batchlyt.setWidget(4, right, self.beginbatchbtn)
+        self.batchuploadbtn = self.file_btnsetup('Upload CSV')
+        self.batchpathlbl = self.file_lblsetup()
+
+        self.format_trimbtn = QtWidgets.QPushButton("Trimming Format")
+        self.format_vigbtn = QtWidgets.QPushButton("Vignettes Format")
+        self.format_trimbtn.setFixedWidth(110)
+        self.format_vigbtn.setFixedWidth(110)
+
+        lyt = QtWidgets.QGridLayout(uploadbox)
+        lyt.setVerticalSpacing(12)
+        lyt.setHorizontalSpacing(9)
+        lyt.setContentsMargins(3, 9, 3, 9)
+
+        lyt.addWidget(self.batchuploadbtn, 0, 0, 1, 1)
+        lyt.addWidget(self.batchpathlbl, 0, 1, 1, 2)
+        lyt.addWidget(self.format_trimbtn, 1, 1, 1, 1)
+        lyt.addWidget(self.format_vigbtn, 1, 2, 1, 1)
+
+        return uploadbox
+
+    def batch_format_msgbox(self, isVignettes=True):
+        msg = QtWidgets.QMessageBox()
+        msg.setFixedSize(QtCore.QSize(600, 400))
+        msg.setWindowTitle('Batch formatting')
+        msg.setIcon(QtWidgets.QMessageBox.Information)
+        msg.setInformativeText(
+            'Please include the headers\n (not case-sensitive)'
+        )
+        if isVignettes:
+            msg.setIcon(QtWidgets.QMessageBox.Information)
+            msg.setText("Batch CSV for Vignettes")
+            msg.setDetailedText(
+                "KEY:\n" +
+                "Source = The exact location of the raw video\n" +
+                "Beeps = List of all beep moments, in seconds\n" +
+                # "Save As = OPTIONAL, the exact location where to save vignette, otherwise defaults\n\n" +
+                "-------------------------------------------------------\n" +
+                "source              |  " +
+                "beeps              " +
+                # "|   save as\n" +
+                "-------------------------------------------------------\n" +
+                "X:/Input/Video.mp4  |  30, 38.02, 40.333  " +
+                # "|  X:/Input/Noun.mp4\n\n" +
+                "\n\n(produces beeps at 30, 38.02, and 40.333 seconds)"
+            )
+        else:  # Trim mode
+            msg.setIcon(QtWidgets.QMessageBox.Information)
+            msg.setText("Batch CSV for Trimming")
+
+            msg.setDetailedText(
+                "KEY:\n" +
+                "Source = The exact location of video to trim\n" +
+                "Start / End = The exact time in MM:SS:ZZZ format\n\n" +
+                "----------------------------------------\n" +
+                "source             |  " +
+                "start    | end\n" +
+                "----------------------------------------\n" +
+                "X:/Input/Video.mp4 | 00:00:00 | 01:00:00\n\n" +
+                "(produces 1 min long video)"
+            )
+
+        # enables "show details" w/o user needing to click
+        for btn in msg.buttons():  # type: QtWidgets.QAbstractButton
+            btn.click()
+
+        detailsbox = msg.findChild(QtWidgets.QTextEdit)  # type:QtWidgets.QTextEdit
+        if detailsbox:
+            detailsbox.setFont(QtGui.QFont('Courier New', 9))
+            detailsbox.setFixedSize(QtCore.QSize(600, 200))
+
+        print(detailsbox.size())
+
+        msg.exec_()
+
+    def batch_think(self, isVignettes = True):
+        msg = QtWidgets.QDialog()
+
+
 
 class UIMessageBoxes:
     # generic error box for both vignette + trimming
@@ -278,14 +376,18 @@ class UIMessageBoxes:
 
         msg.exec_()
 
-    def permissionerrorbox(self):
+    def permissionerrorbox(self, checking=False):
         msg = QtWidgets.QMessageBox()
         msg.setMinimumWidth(80)
         msg.setWindowTitle('File in Use')
 
         msg.setIcon(QtWidgets.QMessageBox.Warning)
         msg.setText("CSV is currently open")
-        msg.setInformativeText("Make sure no one has it open and try again")
+
+        if checking:
+            msg.setInformativeText('Remember to close it before starting the process')
+        else:
+            msg.setInformativeText("Make sure no one has it open and try again")
 
         msg.exec_()
 
